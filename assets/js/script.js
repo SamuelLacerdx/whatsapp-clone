9deab4d8038739f03443dc77b3d92006bcb5c68b
SamuelLacerdx <samuell.rodri13@gmail.com>
2026-08-23 10:55:19 -0300

    troca de perfil

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-import { contatos, listarUsuarios, mostrarConversa } from "./contatos.js";
+import { contatos, listarUsuarios, mostrarConversa, usuarios } from "./contatos.js";
 
 const elemento = {
   lista_contatos: document.querySelector(".container-mensagens"),
@@ -6,10 +6,15 @@ const elemento = {
   mensagems_perfil: document.querySelector(".contatos"),
   foto_perfil: document.querySelector(".foto-perfil"),
   containerMensagens: document.querySelector(".grid-msg"),
+  fotoUsuario: document.querySelector(".foto-usuario img"),
+  nomeUsuario: document.querySelector(".nome-usuario"),
+  numeroUsuario: document.querySelector(".numero-usuario"),
+  containerPerfisSecundarios: document.querySelector(".container_perfis_secundarios"),
 };
 
+let usuarioAtual = 0; 
+
 function criarContatos(idContato, nome, hora, previa, naolidas) {
-  // criação dos elementos do card contatos
   const cardContato = document.createElement("div");
   const fotoContato = document.createElement("img");
   const nomeContato = document.createElement("p");
@@ -33,16 +38,15 @@ function criarContatos(idContato, nome, hora, previa, naolidas) {
   cardContato.id = idContato;
 
   cardContato.append(fotoContato, nomeContato, horaMsg, previaMsg, msgNaoLidas);
-
   elemento.lista_contatos.append(cardContato);
 
   cardContato.addEventListener("click", () => {
-    mostrarConversa(0, idContato);
+    mostrarConversa(usuarioAtual, idContato);
   });
 }
 
 function renderizarListaContatos(idUsuario) {
-  elemento.lista_contatos.innerHTML = ""; // limpa a lista antes de recriar
+  elemento.lista_contatos.innerHTML = "";
 
   contatos(idUsuario).forEach((element, index) => {
     const mensagensRecebidas = element.messages.filter(
@@ -59,10 +63,52 @@ function renderizarListaContatos(idUsuario) {
   });
 }
 
-renderizarListaContatos(0);
-mostrarConversa(0, 0);
+function atualizarInfoPerfil(idUsuario) {
+  const usuario = usuarios["whats-users"][idUsuario];
 
-// aparecer e desaparecer perfil
+  elemento.fotoUsuario.src = `https://i.pravatar.cc/150?img=${idUsuario + 1}`;
+  elemento.nomeUsuario.innerText = usuario.nickname || usuario.account;
+  elemento.numeroUsuario.innerText = usuario.number;
+}
+
+function criarSelecaoPerfis() {
+  elemento.containerPerfisSecundarios.innerHTML = "";
+
+  usuarios["whats-users"].forEach((usuario, index) => {
+    const cardPerfil = document.createElement("div");
+    const avatarPerfil = document.createElement("img");
+    const nomePerfil = document.createElement("p");
+
+    cardPerfil.className = "card-perfil-secundario";
+    avatarPerfil.className = "avatar-perfil-secundario";
+    nomePerfil.className = "nome-perfil-secundario";
+
+    avatarPerfil.src = `https://i.pravatar.cc/150?img=${index + 1}`;
+    avatarPerfil.alt = usuario.nickname;
+    nomePerfil.innerText = usuario.nickname;
+
+    if (index === usuarioAtual) {
+      cardPerfil.classList.add("ativo");
+    }
+
+    cardPerfil.addEventListener("click", () => {
+      trocarPerfil(index);
+    });
+
+    cardPerfil.append(avatarPerfil, nomePerfil);
+    elemento.containerPerfisSecundarios.append(cardPerfil);
+  });
+}
+
+function trocarPerfil(idUsuario) {
+  usuarioAtual = idUsuario;
+
+  atualizarInfoPerfil(idUsuario);
+  renderizarListaContatos(idUsuario);
+  criarSelecaoPerfis()
+  mostrarConversa(idUsuario, 0);
+}
+
 function mostarPerfil() {
   elemento.foto_perfil.addEventListener("click", (evento) => {
     evento.preventDefault();
@@ -70,4 +116,10 @@ function mostarPerfil() {
     elemento.perfil.classList.toggle("ocultar");
   });
 }
+
 mostarPerfil();
+listarUsuarios();
+atualizarInfoPerfil(usuarioAtual);
+renderizarListaContatos(usuarioAtual);
+criarSelecaoPerfis();
+mostrarConversa(usuarioAtual, 0);
